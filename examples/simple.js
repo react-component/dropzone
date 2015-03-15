da16aa2082a3b4385c8bd645638ef4c3bbfb431d
翰文 <hanwen.sah@taobao.com>
2015-03-15 21:24:48 +0800

    feat: finish assept function

--- examples/simple.js
+++ examples/simple.js
@@ -1,9 +1,16 @@
 // use jsx to render html, do not modify simple.html
 var Dropzone = require('../lib/Dropzone');
-require('./simple.css')
+require('./simple.css');
 var React = require('react');
+function accept(file) {
+  if (file.type.indexOf('image/') > -1) {
+    return Promise.resolve();
+  }
+  return Promise.reject('只允许上传图片文件');
+}
+
 React.render(
-    <Dropzone action="http://127.0.0.1:3000/">
+    <Dropzone action="http://127.0.0.1:3000/" accept={accept}>
       <div className="dz-message">
         Drop files here or click to upload.<br/>
         <span className="note">(This is just a demo dropzone. Selected files are <strong>not</strong> actually uploaded.)</span>
