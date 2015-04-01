22c6ee146177dc1c57b60ca83e7842a7c83bd5ea
翰文 <hanwen.sah@taobao.com>
2015-04-01 15:46:49 +0800

    feat: 修复同一个文件再次上传时候无法触发input的change事件的bug

--- examples/simple.js
+++ examples/simple.js
@@ -10,14 +10,33 @@ function accept(file) {
   return Promise.reject('只允许上传图片文件');
 }
 
-React.render(
-    <Dropzone action="http://127.0.0.1:3000/"
+var data = {type: 'haha'};
+
+var dropzone = React.render(
+  <Dropzone
+    action="http://127.0.0.1:3000/"
     accept={accept}
-    >
-      <div className="dz-message">
-        Drop files here or click to upload.<br/>
-        <span className="note">(This is just a demo dropzone. Selected files are
-          <strong>not</strong> actually uploaded.)</span>
-      </div>
-    </Dropzone>,
-    document.getElementById('__react-content'));
+    success={success}
+    error={error}
+    data={data}
+  >
+    <div className="dz-message">
+      Drop files here or click to upload.<br/>
+      <span className="note">(This is just a demo dropzone. Selected files are
+        <strong>not</strong> actually uploaded.)</span>
+    </div>
+  </Dropzone>,
+  document.getElementById('__react-content')
+);
+
+function success(urls) {
+  console.log(urls);
+}
+
+function error(e, file) {
+  console.log(e);
+  setTimeout(function() {
+    dropzone.remove(file);
+  }, 3000);
+}
+
