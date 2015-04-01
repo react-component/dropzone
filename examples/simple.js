770dda5b97a6a17732a545b0d7fa506c13817872
翰文 <hanwen.sah@taobao.com>
2015-04-01 09:58:52 +0800

    refactor: 使用flux模式开发

--- examples/simple.js
+++ examples/simple.js
@@ -11,7 +11,9 @@ function accept(file) {
 }
 
 React.render(
-    <Dropzone action="http://127.0.0.1:3000/" accept={accept}>
+    <Dropzone action="http://127.0.0.1:3000/"
+    accept={accept}
+    >
       <div className="dz-message">
         Drop files here or click to upload.<br/>
         <span className="note">(This is just a demo dropzone. Selected files are
