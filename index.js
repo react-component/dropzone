704653b87e8ee71291d9a2fe1aa15fa232bd8a00
yiminghe <yiminghe@gmail.com>
2015-04-29 16:41:21 +0800

    clean

--- index.js
+++
@@ -1,2 +0,0 @@
-require('./assets/index.css');
-module.exports = require('./lib/Dropzone');
--- rc.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  saucelabs: {
-    browsers: [
-      {browserName: 'chrome'},
-      {browserName: 'firefox'},
-      {browserName: 'internet explorer', version: 11, platform: 'Windows 8.1'}
-    ]
-  }
-};
